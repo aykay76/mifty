31b3b16a1664c49de882c9b1e002ac2845cebdd7
Alan Kelly <alan.kelly.london@gmail.com>
2021-04-21 09:06:54 +0100

    add query class and type add vscode helpers

---
+++ QueryClass.cs
@@ -0,0 +1,11 @@
+namespace mifty
+{
+    public static class QueryClass
+    {
+        public const byte IN = 1;
+        public const byte CS = 2;
+        public const byte CH = 3;
+        public const byte HS = 4;
+        public const byte All = 255;
+    }
+}
---
+++ QueryType.cs
@@ -0,0 +1,26 @@
+namespace mifty
+{
+    public static class QueryType
+    {
+        public const byte A = 1;
+        public const byte NS = 2;
+        public const byte MD = 3;
+        public const byte MF = 4;
+        public const byte CNAME = 5;
+        public const byte SOA = 6;
+        public const byte MB = 7;
+        public const byte MG = 8;
+        public const byte MR = 9;
+        public const byte NULL = 10;
+        public const byte WKS = 11;
+        public const byte PTR = 12;
+        public const byte HINFO = 13;
+        public const byte MINFO = 14;
+        public const byte MX = 15;
+        public const byte TXT = 16;
+        public const byte AFXR = 252;
+        public const byte MAILB = 253;
+        public const byte MAILA = 254;
+        public const byte All = 255;
+    }
+}
