e05f78e9016493221bd3d3922676d07210fc5e32
Alan Kelly <alan.kelly.london@gmail.com>
2021-04-27 21:43:29 +0100

    notes for later

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 
 namespace mifty
@@ -7,6 +9,41 @@ namespace mifty
     {
         static void Main(string[] args)
         {
+            // temporary, i want to do some analysis of the block list to see how evenly distributed the entries are
+            // Dictionary<char, Dictionary<char, int>> frequencies = new Dictionary<char, Dictionary<char, int>>();
+            // StreamReader reader = new StreamReader("dnscrypt-proxy.blacklist.txt");
+            // string line = string.Empty;
+            // while ((line = reader.ReadLine()) != null)
+            // {
+
+            //     if (frequencies.ContainsKey(line[0]) == false)
+            //     {
+            //         frequencies.Add(line[0], new Dictionary<char, int>());
+            //     }
+
+            //     if (frequencies[line[0]].ContainsKey(line[1]))
+            //     {
+            //         frequencies[line[0]][line[1]]++;
+            //     }
+            //     else
+            //     {
+            //         frequencies[line[0]].Add(line[1], 1);
+            //     }
+            // }
+
+            // foreach (KeyValuePair<char, Dictionary<char, int>> kvp in frequencies)
+            // {
+            //     foreach (KeyValuePair<char, int> innerKvp in kvp.Value)
+            //     {
+            //         Console.WriteLine($"{kvp.Key} => {innerKvp.Key} => {innerKvp.Value}");
+            //     }
+            // }
+            // return;
+
+            // ok, so loading into a rough tree separating the first two characters as the first and second level branches isn't very balanced, but it's possibly fast enough
+            // can look at optimising this later with a balanced tree for better speed, although to be fair this processes all 280,000 rows pretty quickly anyway
+            // TODO: create a tree structure to hold the domains in the block list for a quicker decision on whether to block
+
             var exitEvent = new ManualResetEvent(false);
 
             Console.CancelKeyPress += (sender, eventArgs) => {
