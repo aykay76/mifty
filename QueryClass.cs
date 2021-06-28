1c2ff5897ba125cebf61d7d9d6bcfdcbf7dac15c
Alan Kelly <alan.kelly.london@gmail.com>
2021-06-28 13:41:57 +0100

    make classes easier to read

--- QueryClass.cs
+++ QueryClass.cs
@@ -2,11 +2,10 @@ namespace mifty
 {
     public static class QueryClass
     {
-        // TODO: investigate Chaos class - sounds interesting!!
-        public const ushort IN = 1;
-        public const ushort CS = 2;
-        public const ushort CH = 3;
-        public const ushort HS = 4;
+        public const ushort Internet = 1;
+        public const ushort CSNet = 2;
+        public const ushort Chaos = 3;
+        public const ushort Hesiod = 4;
         public const ushort All = 255;
     }
 }
